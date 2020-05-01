a8b12e62b697c854adcfbd765b8212c54a428840
endziu <koper.andrzej@gmail.com>
2020-05-01 11:44:42 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+function App() {
+  return (
+    <div className="container mx-auto">
+      <div className="flex justify-center items-center w-full h-screen bg-green-700">
+        <p className="text-white text-4xl">create-react-app + tailwindcss</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
